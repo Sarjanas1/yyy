710bece3f9b527380c9cdc81fc3dc31632277d3f
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-10-24 14:32:22 +0700

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -21,6 +21,7 @@ from YukkiMusic.core.call import Yukki
 from YukkiMusic.plugins import ALL_MODULES
 from YukkiMusic.utils.database import get_banned_users, get_gbanned
 from YukkiMusic.plugins.tools.tagall import client
+
 loop = asyncio.get_event_loop()
 
 
